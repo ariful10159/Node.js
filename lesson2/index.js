78a29f5d8017b766b01d593069fcf2514e1d07aa
ariful10159 <arifulislammasum97@gmail.com>
2026-01-18 21:07:43 +0600

    initial commit

--- lesson2/index.js
+++ lesson2/index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 
 // How to create and write file using asynchronous method
-fs.writeFile("demo1.txt", "My name is Anisul Islam", (err)=>{
+fs.writeFile("demo1.txt", "My name is Ariful Islam", (err)=>{
     if(err){
         console.log(err);
     }else{
@@ -10,23 +10,31 @@ fs.writeFile("demo1.txt", "My name is Anisul Islam", (err)=>{
     }
 });
 
-// How to create and write file using synchronous method
-var result = fs.writeFileSync("demo1.txt", "My name is Anisul Islam");
-    if(result){
-        console.log(result);
-    }else{
-        console.log("Successful")
-    }
-
-
-// How to update file
-fs.appendFile("demo1.txt", ". I am 30 years old", (err)=>{
+fs.appendFile("demo1.txt", " .i am a cse student", (err)=>{
     if(err){
         console.log(err);
     }else{
-        console.log("Updated Successful")
+        console.log("Successful")
     }
-})
+});
+
+// // How to create and write file using synchronous method
+// var result = fs.writeFileSync("demo1.txt", "My name is Ariful Islam");
+//     if(result){
+//         console.log(result);
+//     }else{
+//         console.log("Successful")
+//     }
+
+
+// // How to update file
+// fs.appendFile("demo1.txt", ". I am 30 years old", (err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Updated Successful")
+//     }
+// })
 
 // How to read file
 fs.readFile("demo1.txt", 'utf-8', (err, data)=>{
@@ -47,20 +55,20 @@ fs.rename("demo1.txt", 'demo2.txt', (err)=>{
 })
 
 
-// How to delete file
-fs.unlink('demo2.txt', (result)=>{
-    if(result){
-        console.log("successfully deleted");
-    }else{
-        console.log("successfully not deleted")
-    }
-})
+// // How to delete file
+// fs.unlink('demo2.txt', (result)=>{
+//     if(result){
+//         console.log("successfully deleted");
+//     }else{
+//         console.log("successfully not deleted")
+//     }
+// })
 
-// How to check the existance
-fs.exists('demo2.txt', (result)=>{
-    if(result){
-        console.log("found");
-    }else{
-        console.log("not found")
-    }
-})
+// // How to check the existance
+// fs.exists('demo2.txt', (result)=>{
+//     if(result){
+//         console.log("found");
+//     }else{
+//         console.log("not found")
+//     }
+// })
